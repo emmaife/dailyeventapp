ec21d928130bfda71e98b8bbb1821a4db328888e
justinbelmont <justinbelmont@gmail.com>
2014-04-01 14:01:33 -0400

    Updated for random event on homepage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,8 @@
 class EventsController < ApplicationController
 
   def index
-    # @event = Event.new
+
     @event = Event.new
-    # @event = Event.new
 
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,17 +2,26 @@ class Event < ActiveRecord::Base
 attr_accessor :event_names, :parsed_response, :response
 
 require 'rest_client'
+
+EVENT_SELECTION = rand(0..55)
+
 def initialize
   response = RestClient.get 'http://www.kimonolabs.com/api/bo0xm0eg?apikey=605ac5df894ed1e51bc760ce3f0daa17'
 
-@parsed_response = JSON.parse(response)
+@parsed_response = JSON.parse(response)['results']['collection1']
 end
 
+
+
   def event_names
-     @parsed_response['results']['collection1'].map {|thing| thing["event_name"]}
+     @parsed_response.map {|thing| thing["event_name"]}[EVENT_SELECTION]
   end
 
+  def description
+    @parsed_response.map {|thing| thing["description"]}[EVENT_SELECTION]
+  end
 
-
-
+  def url
+    @parsed_response.map {|thing| thing["website"]["href"]}[EVENT_SELECTION]
+  end
 end
