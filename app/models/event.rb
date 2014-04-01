51014a21af03b2916deab68add8be08e7c079dec
justinbelmont <justinbelmont@gmail.com>
2014-04-01 15:07:02 -0400

    Adding functionality to eliminate bad data from description

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,20 +8,23 @@ EVENT_SELECTION = rand(0..55)
 def initialize
   response = RestClient.get 'http://www.kimonolabs.com/api/bo0xm0eg?apikey=605ac5df894ed1e51bc760ce3f0daa17'
 
-@parsed_response = JSON.parse(response)['results']['collection1']
+  @parsed_response = JSON.parse(response)['results']['collection1']
+
+  @parsed_response.delete_if{ |i| i["description"] == "" || i["description"].length < 50 || i["description"].is_a?(Hash)}
+
+
 end
 
-
-
   def event_names
-     @parsed_response.map {|thing| thing["event_name"]}[EVENT_SELECTION]
+     @parsed_response.map {|thing| thing["event_name"]}[19]
   end
 
   def description
-    @parsed_response.map {|thing| thing["description"]}[EVENT_SELECTION]
+    @parsed_response.map {|thing| thing["description"]}[19]
   end
 
   def url
     @parsed_response.map {|thing| thing["website"]["href"]}[EVENT_SELECTION]
   end
 end
+
