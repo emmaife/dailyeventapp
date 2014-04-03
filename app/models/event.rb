465c07e78cc9160d3b5582655cba83eb194db379
emmaife <emma.s.ife@gmail.com>
2014-04-03 13:35:39 -0400

    pushing updates

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,39 +9,44 @@ require 'rest_client'
     @user_neighborhoods = user_neighborhoods
     @user_categories = user_categories
     @event_selection = 0
-    @user_selection = 0
+    @hood_select = 0
+    @category_select = 0
+
+
 
     response = RestClient.get 'http://www.kimonolabs.com/api/bo0xm0eg?apikey=605ac5df894ed1e51bc760ce3f0daa17'
     @parsed_response = JSON.parse(response)['results']['collection1'].delete_if{ |i| i["description"] == "" || i["description"].length < 50 || i["description"].is_a?(Hash)}
-    @user_choice = @parsed_response.select {|i| i["neighborhood"] == user_neighborhoods.pluck(:name)[@user_selection]}
-    while @user_choice.empty?
-            @user_selection += 1
-            if @user_selection <= user_neighborhoods.count
-            @user_choice = @parsed_response.select { |i| i["neighborhood"] == user_neighborhoods.pluck(:name)[@user_selection]}
-          elsif @user_selection > user_neighborhoods.count
-            @user_choice = @parsed_response.select { |i| i["neighborhood"] == "Midtown West"}
+    
+    @result = @parsed_response.select {|i| i["neighborhood"] == user_neighborhoods.pluck(:name)[@hood_select]}
+    
+    while @result.empty?
+      @hood_select += 1
+      if @hood_select <= user_neighborhoods.count
+        @result = @parsed_response.select { |i| i["neighborhood"] == user_neighborhoods.pluck(:name)[@hood_select]}
+      elsif @hood_select > user_neighborhoods.count
+            @result = @parsed_response.select { |i| i["neighborhood"] == "Midtown West"}
           end
         end
       end
 
   def event_name
-     @user_choice.map {|thing| thing["event_name"]}[@event_selection]
+     @result.map {|thing| thing["event_name"]}[@event_selection]
   end
 
   def description
-    @user_choice.map {|thing| thing["description"]}[@event_selection]
+    @result.map {|thing| thing["description"]}[@event_selection]
   end
 
   def url
-    @user_choice.map {|thing| thing["website"]["href"]}[@event_selection]
+    @result.map {|thing| thing["website"]["href"]}[@event_selection]
   end
 
   def pic
-    @user_choice.map {|thing| thing["pic"]["src"]}[@event_selection]
+    @result.map {|thing| thing["pic"]["src"]}[@event_selection]
   end
 
   def neighborhood
-    @user_choice.map {|thing| thing["neighborhood"]}[@event_selection]
+    @result.map {|thing| thing["neighborhood"]}[@event_selection]
   end
 
 
