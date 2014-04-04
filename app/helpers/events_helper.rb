551c10307f5bc2eedffc7a11beec8f351232d337
emmaife <emma.s.ife@gmail.com>
2014-04-04 12:07:40 -0400

    css

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,10 @@
+module EventsHelper 
+
+#   def hood_organizer
+#     @manhattan = ["East Village", "West Village", "Lower East Side", "Chinatown", "Soho", "Greenwich Village", "Gramercy & Flatiron", "Meatpacking District", "Tribeca", "Chelsea", "Financial District", "Midtown East", "Midtown West", "Upper East Side", "Upper West Side", "Harlem"]
+#     @brooklyn = ["Dumbo", 'Williamsburg', 'Greenpoint', 'Bushwick', 'Prospect Heights', 'Fort Greene', 'Gowanus', 'Park Slope', 'Prospect Park', 'Brooklyn Heights', 'Boerum Hill', 'Red Hook'] 
+#     @queens = ['Long Island City', 'Astoria']
+    
+  end
+
+
