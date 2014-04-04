35e89fd6beafcc6ecda990cffca38602527edaa5
justinbelmont <justinbelmont@gmail.com>
2014-04-04 14:37:08 -0400

    Final changes for minimum viable application

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,17 @@
 module EventsHelper 
 
-#   def hood_organizer
-#     @manhattan = ["East Village", "West Village", "Lower East Side", "Chinatown", "Soho", "Greenwich Village", "Gramercy & Flatiron", "Meatpacking District", "Tribeca", "Chelsea", "Financial District", "Midtown East", "Midtown West", "Upper East Side", "Upper West Side", "Harlem"]
-#     @brooklyn = ["Dumbo", 'Williamsburg', 'Greenpoint', 'Bushwick', 'Prospect Heights', 'Fort Greene', 'Gowanus', 'Park Slope', 'Prospect Park', 'Brooklyn Heights', 'Boerum Hill', 'Red Hook'] 
-#     @queens = ['Long Island City', 'Astoria']
-    
+  def queens
+    ['Long Island City', 'Astoria']
   end
+
+  def manhattan
+    ["East Village", "West Village", "Lower East Side", "Chinatown", "Soho", "Greenwich Village", "Gramercy & Flatiron", "Meatpacking District", "Tribeca", "Chelsea", "Financial District", "Midtown East", "Midtown West", "Upper East Side", "Upper West Side", "Harlem"]
+  end
+
+  def brooklyn
+     ["Dumbo", 'Williamsburg', 'Greenpoint', 'Bushwick', 'Prospect Heights', 'Fort Greene', 'Gowanus', 'Park Slope', 'Prospect Park', 'Brooklyn Heights', 'Boerum Hill', 'Red Hook'] 
+  end
+
+end
 
 
